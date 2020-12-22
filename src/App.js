5780af03f5dd0cc8ac3d98420c640f4e7d2f6372
unknown <thomasbakkenmoe@gmail.com>
2020-12-22 21:51:26 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDsJeeJTAr-XEM2wMbPu2A7dkiED9kIFHU",
+  authDomain: "quest-terminal.firebaseapp.com",
+  databaseURL: "https://quest-terminal.firebaseio.com",
+  projectId: "quest-terminal",
+  storageBucket: "quest-terminal.appspot.com",
+  messagingSenderId: "325595410332",
+  appId: "1:325595410332:web:fe79f39fbcabb8dcb9a259"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <section>
+        {user ? <SignIn /> : <SignIn />}
+      </section>
     </div>
   );
 }
 
+function SignIn() {
+
+  const signInWithMail = () => {
+    auth.signInWithEmailAndPassword("izabelle_nybrodahl@hotmail.com", "VY$7A*3Z");
+    console.log("Logged in!");
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithMail}>Sign in</button>
+    </>
+  )
+}
 export default App;
